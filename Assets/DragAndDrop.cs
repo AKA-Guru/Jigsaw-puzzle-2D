a817932a43dca01bc6205570c37ff66d6d1740f7
AKA-Guru <AkaGameGuru@gmail.com>
2021-09-16 22:56:41 +0500

    added drag and drop using raycast 2d

--- Assets/DragAndDrop.cs
+++ Assets/DragAndDrop.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
+    [SerializeField] private GameObject selectedpiece;
         
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,26 @@ public class DragAndDrop : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit2D hitPiece= Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition),Vector2.zero)
+            RaycastHit2D hitPiece =
+                Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hitPiece.transform.CompareTag("Puzzle Piece"))
+
+            {
+                selectedpiece = hitPiece.transform.gameObject;
+            }
+
+        
+            
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectedpiece = null;
+        }
+
+        if (selectedpiece != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            selectedpiece.transform.position = new Vector3(mousePos.x, mousePos.y, 0f);
         }
         
     }
